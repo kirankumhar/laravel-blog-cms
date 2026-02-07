f9c39ba355c2ecf3a63e5e9bc65c8a58e7adcba4
kirankumhar <kiran.kumar91@live.com>
2026-02-07 22:22:09 +0530

    dashboard route + dasboard blade

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('dashboard');
+});
